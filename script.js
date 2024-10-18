e1886fbdaad9f3452133eed099fac86a28e59de4
MISHOW2 <mshibambo63@gmail.com>
2024-10-18 17:22:38 +0200

    finished

---
+++ script.js
@@ -0,0 +1,18 @@
+let openMenu =document.querySelector('.fa-solid');
+let menu = document.querySelector('.navigation');
+
+let closeMenu = document.querySelector('.closeMenu')
+
+openMenu.addEventListener("click", ()=>{
+   menu.style.display = "block";
+   openMenu.style.display ="none"
+  closeMenu.style.display ="block"
+}
+)
+
+closeMenu.addEventListener("click", ()=>{
+  menu.style.display = "none";
+  openMenu.style.display ="block"
+  closeMenu.style.display ="none"
+}
+)
